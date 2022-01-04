88425950e84bc045d90a80098e4a7ea6898b4217
Kak_i7 <support@gamerbear.com>
2022-01-04 13:15:58 +0100

    initial import

---
+++ AwaitFail2/AsyncCoordinator.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AwaitFail2
+{
+
+
+
+class AsyncCoordinator : INotifyCompletion
+{
+private Queue<Action> actions = new Queue<Action>();				// actions a executar
+private Queue<Action> actionsToBeContinued = new Queue<Action>();	// quan s'acaben d'executar, es tornen a posar a actions2 si cal
+
+public delegate Task AsyncSequenceFunction (AsyncCoordinator coordinator);
+
+
+public void scheduleCoroutine (AsyncSequenceFunction func)
+{
+	actions.Enqueue (
+		async ()=>
+		{
+			await func (this);
+		});
+}
+
+public bool RunStep ()
+{
+
+	if (actions.Count<=0 && actionsToBeContinued.Count<=0) return false;
+
+	while (actions.Count>0)
+	{
+		// Debug note: Es impossible fer un step in cap a dins, pero podem posar un breakpoint al OnComplete
+		// per saber on es amb el stack trace
+		var act=actions.Dequeue();
+		act.Invoke(); 
+	}
+
+	// swap
+	swap (ref actions,ref actionsToBeContinued);
+
+
+	return true;
+
+}
+
+
+void swap<T> (ref T x,ref T y)
+{
+	T tmp=x;
+	x=y;
+	y=tmp;
+}
+
+
+#region await stuff
+
+
+
+public void OnCompleted (Action continuation)
+{
+	actionsToBeContinued.Enqueue (continuation);
+}
+
+
+public AsyncCoordinator GetAwaiter ()
+{
+	return this;
+}
+
+
+public bool IsCompleted { get { return false; }}
+
+
+public void GetResult ()
+{
+	// we have no result.
+}
+
+#endregion
+
+
+}
+
+}
---
+++ AwaitFail2/AwaiterCoordinatorTest2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AwaitFail2
+{
+
+static class AwaiterCoordinatorTest2
+{
+static AsyncCoordinator coordinator;
+
+//=============================================================================
+/// <summary></summary>
+public static void run ()
+{
+	coordinator=new AsyncCoordinator();
+
+	coordinator.scheduleCoroutine (w_doJob1);
+
+	while (RunStep())
+	{
+		//Console.WriteLine ("Step ran");
+	}
+
+	Console.WriteLine ("No more steps to run");
+}
+
+//=============================================================================
+/// <summary></summary>
+static bool RunStep ()
+{
+	return coordinator.RunStep();
+}
+
+//=============================================================================
+/// <summary></summary>
+static async Task extraAwaitableFunc (AsyncCoordinator coord)
+{
+int i;
+
+	for (i=0;i<2;i++)
+	{
+		Console.WriteLine ("**** subwait "+i);
+		await coord;
+	}
+}
+
+
+static async Task w_doJob1 (AsyncCoordinator coord)
+{
+	Console.WriteLine ("**   step 1");
+	await coord;
+
+	Console.WriteLine ("**   step 2");
+	await extraAwaitableFunc(coord);
+
+	Console.WriteLine ("**   step 3");
+	await coord;
+}
+
+
+}
+
+
+
+}
